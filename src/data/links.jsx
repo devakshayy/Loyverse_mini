d39ea4aa78022a4cd888be9c94dbd88890784873
devakshayy <devakshay916@gmail.com>
2025-01-31 18:14:44 +0530

    the titel head change as location

--- src/data/links.jsx
+++ src/data/links.jsx
@@ -7,103 +7,7 @@ import { IoExtensionPuzzle } from "react-icons/io5";
 import { IoMdSettings } from "react-icons/io";
 import { AiOutlineQuestionCircle } from "react-icons/ai";
 
-
-
-
-// export const links = [
-//     {
-//       title: 'Reports',
-//       icon: <BsBarChartLineFill />,
-//       path: "/reports",
-//       links: [
-//         {
-//            link1: 'Sales summary',
-//            link2: 'Sales by item',
-//            link3: 'Sales by category',
-//            link4: 'Sales by employee',
-         
-//         },
-//       ],
-//     },
-//     {
-//         title: 'Items',
-//         icon: <GiBeachBag />,
-//         path: "/items",
-//         links: [
-//               {
-//                 name: 'Item list',
-//               },
-//               {
-//                 name: 'Categories',
-//               },
-//               {
-//                 name: 'Modifiers',
-//               },
-//               {
-//                 name: 'Discounts',
-//               },
-//         ],
-//       },
-//       {
-//         title: 'Inventry management',
-//         icon: <RiLuggageCartLine />,
-//         path: "/inventrymanagement",
-//       },
-//       {
-//         title: 'Employees',
-//         icon: <LiaIdCard />,
-//         path: "/employees",
-//         links: [
-//           {
-//             name:'Employee list',
-//           },
-//           {
-//             name: 'Access rights',
-//           }
-//        ],
-//       },
-//       {
-//         title: 'Customers',
-//         icon: <HiUsers />,
-//         path: "/customers",
-//       },
-//       {
-//         title: 'Integrations',
-//         icon: <IoExtensionPuzzle />,
-//         path: "/integrations",
-//         links: [
-//           {
-//             name:'Apps',
-//           },
-//           {
-//             name: 'Access tokens',
-//           }
-//        ],
-//       },
-//       {
-//         title: 'Settings',
-//         icon: <IoMdSettings />,
-//         path: "/settings",
-//       },
-//       {
-//         title: 'Help',
-//         icon: <AiOutlineQuestionCircle />,
-//         path: "/help",
-//       },
-//   ];
-
 export const links = [
-  {
-    title: 'Reports',
-    icon: <BsBarChartLineFill />,
-    path: "/reports",
-    links: [
-      { name: 'Sales summary' },
-      { name: 'Sales by item' },
-      { name: 'Sales by category' },
-      { name: 'Sales by employee' },
-    ],
-  },
   {
     title: 'Items',
     icon: <GiBeachBag />,
@@ -114,11 +18,6 @@ export const links = [
       { name: 'Modifiers' },
       { name: 'Discounts' },
     ],
-  },
-  {
-    title: 'Inventry management',
-    icon: <RiLuggageCartLine />,
-    path: "/inventrymanagement",
   },
   {
     title: 'Employees',
@@ -133,6 +32,22 @@ export const links = [
     title: 'Customers',
     icon: <HiUsers />,
     path: "/customers",
+  },
+  {
+    title: 'Reports',
+    icon: <BsBarChartLineFill />,
+    path: "/reports",
+    links: [
+      { name: 'Sales summary' },
+      { name: 'Sales by item' },
+      { name: 'Sales by category' },
+      { name: 'Sales by employee' },
+    ],
+  },
+  {
+    title: 'Inventry management',
+    icon: <RiLuggageCartLine />,
+    path: "/inventrymanagement",
   },
   {
     title: 'Integrations',
